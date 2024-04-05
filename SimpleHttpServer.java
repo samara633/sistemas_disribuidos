a293ee1b9581b11c2e8de0b4020b7387ea161e52
samara633 <samara20031012@gmail.com>
2024-04-05 15:06:05 -0500

    primera implementacion

--- SimpleHttpServer.java
+++ SimpleHttpServer.java
@@ -15,7 +15,7 @@ public class SimpleHttpServer {
         int serverPort = 8000;
         HttpServer server = HttpServer.create(new InetSocketAddress(serverPort), 0);
         server.createContext("/calcular", new CalculadoraHandler());
-        server.setExecutor(null); // creates a default executor
+        server.setExecutor(null);
         server.start();
         System.out.println("Server started on port " + serverPort);
     }
@@ -32,35 +32,10 @@ public class SimpleHttpServer {
                     arreglo[i] = i + 1;
                 }
 
-                int parteArreglo = arreglo.length / numThreads;
-                int resto = arreglo.length % numThreads;
-
-                SumadorHilo[] sumadores = new SumadorHilo[numThreads];
-                int inicio = 0;
-                int fin = 0;
-
-                for (int i = 0; i < numThreads; i++) {
-                    if (i < resto) {
-                        fin = inicio + parteArreglo + 1;
-                    } else {
-                        fin = inicio + parteArreglo;
-                    }
-                    sumadores[i] = new SumadorHilo(arreglo, inicio, fin);
-                    inicio = fin;
-                }
-
+                // Calcular la suma del arreglo como antes
                 int sumaTotal = 0;
-                for (SumadorHilo sumador : sumadores) {
-                    sumador.start();
-                }
-
-                for (SumadorHilo sumador : sumadores) {
-                    try {
-                        sumador.join();
-                        sumaTotal += sumador.getSuma();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+                for (int valor : arreglo) {
+                    sumaTotal += valor;
                 }
 
                 String response = "La suma total es: " + sumaTotal;
@@ -87,31 +62,6 @@ public class SimpleHttpServer {
                 }
             }
             return formData;
-        }
-    }
-
-    static class SumadorHilo extends Thread {
-        private int[] arreglo;
-        private int inicio;
-        private int fin;
-        private int suma;
-
-        public SumadorHilo(int[] arreglo, int inicio, int fin) {
-            this.arreglo = arreglo;
-            this.inicio = inicio;
-            this.fin = fin;
-        }
-
-        @Override
-        public void run() {
-            suma = 0;
-            for (int i = inicio; i < fin; i++) {
-                suma += arreglo[i];
-            }
-        }
-
-        public int getSuma() {
-            return suma;
         }
     }
 }
